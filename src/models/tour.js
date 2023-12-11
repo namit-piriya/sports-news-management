2c72446deb39cdeaa8e4cbf386aa9607a8680b3f
Namit Piriya <34130842+namit-piriya@users.noreply.github.com>
2023-12-11 11:40:10 +0530

    pick tour from the tours table.

--- src/models/tour.js
+++ src/models/tour.js
@@ -7,7 +7,7 @@ const getAllTours = async () => {
 }
 
 const getMatchesByTourName = async params => {
-    const statement = 'select * from matches left join tours on matches.tourId = tours.id where tours.name = ?';
+    const statement = 'select * from tours where tours.name = ?';
     const parameters = [ params.name ];
     return await mysql.query(statement, parameters);
 }
