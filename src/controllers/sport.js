a3705eb74f829d29c94dce795b3d585bf765126e
Namit Piriya <34130842+namit-piriya@users.noreply.github.com>
2023-12-11 11:37:21 +0530

    add match info in getAllSportsToursAndMatches

--- src/controllers/sport.js
+++ src/controllers/sport.js
@@ -4,14 +4,14 @@ const getAllSportsToursAndMatches = async () => {
     const matches = await Sport.getAllSportsToursAndMatches();
     const res = {};
     matches.forEach(match => {
-        const { sportName, tourName, matchName } = match;
+        const {sportName, tourName, matchName, matchId, format, startTime} = match;
         if (!res[sportName]) {
             res[sportName] = {};
         }
         if (!res[sportName][tourName]) {
             res[sportName][tourName] = [];
         }
-        res[sportName][tourName].push(matchName);
+        res[sportName][tourName].push({matchName, matchId, format, startTime});
     });
     return res;
 }
